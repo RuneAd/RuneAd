a4be2fb23bc39dd3187526c87a13de145f4516f2
Aaron <Spaceage95@hotmail.com>
2020-06-19 16:53:38 -0500

    Removed a few files & general updates
    Removed a few files & general updates

--- public/js/serverads.js
+++ public/js/serverads.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     let Ads = [
       '<a href="https://discord.gg/vmyHKy4" target="_blank"><img alt="Turmoil" class="responsive-ad" src="https://cdn.discordapp.com/attachments/702213830360563783/712411383152771113/728x90.gif"></a>',
       '<a href="http://sinhaza.com/forums/index.php?/topic/16-welcome-to-sinhaza/" target="_blank"><img alt="Shinhaza" class="responsive-ad" src="https://cdn.discordapp.com/attachments/710641741258293289/713072012532383855/728x90.gif"></a>',
-      '<a href="https://discord.gg/kf5AjNw" target="_blank"><img alt="SnowPS" class="responsive-ad" src="https://cdn.discordapp.com/attachments/698524813437960242/701602535861125180/BannerNew.gif"></a>',
+
     ];
 
     let AdBanner = $('#AdBanner');
