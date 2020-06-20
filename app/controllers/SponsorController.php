a4ed6a91d571b66e272d0dea3232cc5492e17a1f
Aaron <Spaceage95@hotmail.com>
2020-06-20 00:35:36 -0500

    more js tweaks
    just some more js tweaks, and disabled sponsor page js for now until it works

--- app/controllers/SponsorController.php
+++ app/controllers/SponsorController.php
@@ -1,16 +1,19 @@
 <?php
  class SponsorController extends Controller {
 
+     private static $disabled = true;
+
      public function index() {
          $packages = SponsorPackages::where('visible', 1)->get();
 
-         if ($this->user) {
-             $servers = Servers::getServersByOwner($this->user->user_id);
+        if ($this->user) {
+            $servers = Servers::getServersByOwner($this->user->user_id);
              $this->set("servers", $servers);
          }
 
+         $this->set("page_disabled", self::$disabled);
          $this->set("packages", $packages);
          return true;
      }
 
- }
+  }
