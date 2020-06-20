e4b5b0ade886d48d9794db97273b4635172e41b5
Aaron <Spaceage95@hotmail.com>
2020-06-19 19:32:30 -0500

    Ad updates
    Ad updates

--- public/js/serverads.js
+++ public/js/serverads.js
@@ -22,3 +22,28 @@ $(document).ready(function() {
     console.log(random);
 
 });
+
+$(document).ready(function() {
+    let #VoteAdBanner = [
+      '<a href="https://discord.gg/vmyHKy4" target="_blank"><img alt="Turmoil" class="responsive-ad" src="https://cdn.discordapp.com/attachments/702213830360563783/712411383152771113/728x90.gif"></a>',
+      '<a href="http://sinhaza.com/forums/index.php?/topic/16-welcome-to-sinhaza/" target="_blank"><img alt="Shinhaza" class="responsive-ad" src="https://cdn.discordapp.com/attachments/710641741258293289/713072012532383855/728x90.gif"></a>',
+
+    ];
+
+    let #VoteAdBanner = $('#VoteAdBanner');
+
+    let random = Math.floor(Math.random() * (#VoteAdBanner.length - 0)) + 0;
+
+    #VoteAdBanner.html(#VoteAdBanner[random]);
+
+    setInterval(function() {
+        random = Math.floor(Math.random() * (#VoteAdBanner.length - 0)) + 0;
+        $('#factBox div').fadeOut(function() {
+            $(this).fadeIn();
+            #VoteAdBanner.html(#VoteAdBanner[random]);
+        })
+    }, 6500);
+
+    console.log(random);
+
+});
