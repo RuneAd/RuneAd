9174e4d6006b07812fdcf3dd5fe398e138f562d0
Aaron <Spaceage95@hotmail.com>
2020-06-24 19:31:50 -0500

    updates

--- public/js/voteads.js
+++ public/js/voteads.js
@@ -1,19 +1,19 @@
 $(document).ready(function() {
-    let Ads = [
+    let VoteAds = [
     '<a href="https://discord.gg/vmyHKy4" target="_blank"><img alt="Turmoil" class="responsive-ad" src="https://cdn.discordapp.com/attachments/702213830360563783/712411383152771113/728x90.gif"></a>',
     '<a href="http://sinhaza.com/forums/index.php?/topic/16-welcome-to-sinhaza/" target="_blank"><img alt="Shinhaza" class="responsive-ad" src="https://cdn.discordapp.com/attachments/710641741258293289/713072012532383855/728x90.gif"></a>',
 
    ];
 
     let AdVotePage = $('#AdVotePage');
-    let random = Math.floor(Math.random() * (Ads.length - 0)) + 0;
-    AdVotePage.html(Ads[random]);
+    let random = Math.floor(Math.random() * (VoteAds.length - 0)) + 0;
+    AdVotePage.html(VoteAds[random]);
 
     setInterval(function() {
-        random = Math.floor(Math.random() * (Ads.length - 0)) + 0;
+        random = Math.floor(Math.random() * (VoteAds.length - 0)) + 0;
         $('#factBox div').fadeOut(function() {
             $(this).fadeIn();
-            AdVotePage.html(Ads[random]);
+            AdVotePage.html(VoteAds[random]);
         })
     }, 6500);
     console.log(random);
