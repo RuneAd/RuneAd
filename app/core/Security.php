171af475c77de8c977610160a353580d8efc0d87
Aaron <Spaceage95@hotmail.com>
2020-06-25 00:28:45 -0500

    Update Security.php

--- app/core/Security.php
+++ app/core/Security.php
@@ -20,6 +20,7 @@ class Security {
             'vote'    => ['index', 'addvote'],
             'premium' => ['index'],
             'sponsor' => ['index'],
+            'report'  => ['index'],
             'login'   => ['index', 'discord', 'auth', 'dauth'],
             'errors'  => ['show404', 'show500', 'show401', 'missing'],
             'pages'   => ['docs', 'updates', 'stats', 'terms', 'privacy'],
