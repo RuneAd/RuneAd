c8106d9b34fa55f31f7b7fd81e8136431528694d
Aaron <Spaceage95@hotmail.com>
2020-06-26 18:58:01 -0500

    undo

--- app/core/PageRouter.php
+++ app/core/PageRouter.php
@@ -117,7 +117,7 @@ class PageRouter extends Router {
          $this->all('admin/payments/([0-9]+)', function($page) {
       return $this->setRoute('payments', 'index', [ 'search' => null, 'page' => $page]);
   });
-  $this->all('admin/payments/([A-Za-z0-9\-_]+)', function($search) {
+  $this->all('admin/payments/([A-Za-z0-9\-_\@]+)', function($search) {
              return $this->setRoute('payments', 'index', ['search' => $search, 'page' => 1]);
          });
          $this->all('admin/payments/([A-Za-z0-9\-_]+)/([0-9]+)', function($search, $page) {
@@ -256,6 +256,10 @@ class PageRouter extends Router {
     }
 
     public function getViewPath() {
+      $admin = ['payments', 'sponsors', 'premium', 'banners'];
+  if (in_array($this->getController(), $admin)) {
+      return "admin/{$this->getController()}/{$this->getMethod()}";
+        }
         return $this->getController().'/'.$this->getMethod();
     }
 
