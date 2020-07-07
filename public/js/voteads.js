24b0a5eb513a0fbe8cc560bcac9e7c23e24280b2
Aaron <Spaceage95@hotmail.com>
2020-07-07 16:21:44 -0500

    Update voteads.js

--- public/js/voteads.js
+++ public/js/voteads.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     let VoteAds = [
     '<a href="https://discord.gg/vmyHKy4" target="_blank"><img alt="Turmoil" class="responsive-ad" src="https://cdn.discordapp.com/attachments/702213830360563783/712411383152771113/728x90.gif"></a>',
-    '<a href="http://sinhaza.com/forums/index.php?/topic/16-welcome-to-sinhaza/" target="_blank"><img alt="Shinhaza" class="responsive-ad" src="https://cdn.discordapp.com/attachments/710641741258293289/713072012532383855/728x90.gif"></a>',
+    '<a href="https://azura-rsps.com" target="_blank"><img alt="Azura RSPS" class="responsive-ad" src="https://media.discordapp.net/attachments/708243799448485909/730167768132878456/728x90_with_Effect.gif"></a>',
 
    ];
 
