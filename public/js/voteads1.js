307f380664155172a85dc321342e56569549dcfc
Aaron <Spaceage95@hotmail.com>
2020-08-28 19:41:58 -0500

    voteads

--- public/js/voteads1.js
+++ public/js/voteads1.js
@@ -1,17 +1,17 @@
 $(document).ready(function() {
-    let VoteAds = [
+    let VoteAds1 = [
       '<a href="http://nefariouspkz.com/" target="_self"><img alt="nefariouspkz" class="responsive-ad" src="https://cdn.discordapp.com/attachments/748349075287441478/749007417035063406/image0.gif"></a>',
    ];
 
-    let AdVotePage = $('#AdVotePage');
-    let random = Math.floor(Math.random() * (VoteAds.length - 0)) + 0;
-    AdVotePage.html(VoteAds[random]);
+    let AdVotePage1 = $('#AdVotePage1');
+    let random = Math.floor(Math.random() * (VoteAds1.length - 0)) + 0;
+    AdVotePage1.html(VoteAds1[random]);
 
     setInterval(function() {
-        random = Math.floor(Math.random() * (VoteAds.length - 0)) + 0;
+        random = Math.floor(Math.random() * (VoteAds1.length - 0)) + 0;
         $('#factBox div').fadeOut(function() {
             $(this).fadeIn();
-            AdVotePage.html(VoteAds[random]);
+            AdVotePage1.html(VoteAds1[random]);
         })
     }, 6500);
     console.log(random);
