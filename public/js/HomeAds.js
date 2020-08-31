1c84d126a478ef816085b8588049f1d8080a2a89
Aaron <Spaceage95@hotmail.com>
2020-08-31 13:24:32 -0500

    Small updates

--- public/js/HomeAds.js
+++ public/js/HomeAds.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     let Servers = [
-      '<a href="http://nefariouspkz.com/" target="_self"><img alt="nefariouspkz" class="responsive-ad" src="https://cdn.discordapp.com/attachments/748349075287441478/749007417035063406/image0.gif"></a>',
+      '<a href="http://nefariouspkz.com/" target="_blank"><img alt="nefariouspkz" class="responsive-ad" src="https://cdn.discordapp.com/attachments/748349075287441478/749007417035063406/image0.gif"></a>',
 
     ];
 
