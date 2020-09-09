1b9692848d21323a83449d3de91b57e3af3478de
Aaron <Spaceage95@hotmail.com>
2020-09-09 02:55:45 -0500

    Update ProfileController.php

--- app/controllers/ProfileController.php
+++ app/controllers/ProfileController.php
@@ -3,7 +3,32 @@
 class ProfileController extends Controller {
 
   public function stats() {
-    $servers = Servers::where('server_id', $this->votes->server_id)->get();
+    $servers = Servers::where('owner', $this->user->user_id)->get();
+    $roles = implode(", ", json_decode($this->user->roles, true));
+
+    $this->set("roles", $roles);
+    $this->set("servers", $servers);
+    return true;
+
+    $thisMonth = strtotime(date("Y-m-01 00:00:00"));
+    $lastMonth = strtotime("first day of last month");
+    $lastWeek = strtotime("-1 week +1 day");
+    $day = strtotime("today");
+    $hour = strtotime("-1 hour");
+
+
+    $data = [
+        'votes' => [
+            'total' => Votes::count(),
+            'month' => Votes::where("votes", ">=", $thisMonth)->count(),
+            'lastmonth' => Votes::where("votes", ">=", $lastMonth)->count(),
+            'week' => Votes::where("votes", ">=", $lastWeek)->count(),
+            'day' => Votes::where("votes", ">=", $day)->count(),
+            'hour' => Votes::where("votes", ">=", $hour)->count()
+
+        ],
+
+    ];
 
   }
 
