d2c6126e5da7c3c4638f473db032d04c39b6d717
Aaron <Spaceage95@hotmail.com>
2020-09-09 02:17:26 -0500

    Update AdminController.php

--- app/controllers/AdminController.php
+++ app/controllers/AdminController.php
@@ -6,7 +6,7 @@ class AdminController extends Controller {
         $lastMonth = strtotime("first day of last month");
         $lastWeek = strtotime("-1 week +1 day");
         $day = strtotime("today");
-        $hour = strtotime("-1 hour");
+        $hour = strtotime(date("-1 hour"));
 
         $data = [
             'users' => [
@@ -35,41 +35,6 @@ class AdminController extends Controller {
                 'month' => Servers::where("date_created", ">=", $thisMonth)->count(),
             ]
         ];
-
-
-        public function users() {
-            $thisMonth = strtotime(date("Y-m-01 00:00:00"));
-            $lastMonth = strtotime("first day of last month");
-            $lastWeek = strtotime("-1 week +1 day");
-            $day = strtotime("today");
-            $hour = strtotime("-1 hour");
-
-            $data = [
-                'users' => [
-                    'total' => Users::results(),
-                ],
-                'votes' => [
-                    'total' => Votes::count(),
-                    'month' => Votes::where("voted_on", ">=", $thisMonth)->count(),
-                    'lastmonth' => Votes::where("voted_on", ">=", $lastMonth)->count(),
-                    'week' => Votes::where("voted_on", ">=", $lastWeek)->count(),
-                    'day' => Votes::where("voted_on", ">=", $day)->count(),
-                    'hour' => Votes::where("voted_on", ">=", $hour)->count()
-
-                ],
-                'reports' => [
-                    'total' => Reports::count(),
-                    'month' => Reports::where("date_reported", ">=", $thisMonth)->count()
-                ],
-                'payments' => [
-                    'total' => Payments::sum('paid'),
-                    'month' => Payments::where("date_paid", ">=", $thisMonth)->sum('paid'),
-                ],
-                'servers' => [
-                    'total' => Servers::count(),
-                    'month' => Servers::where("date_created", ">=", $thisMonth)->count(),
-                ]
-            ];
 
         $chartData = Payments::getChartData();
 
