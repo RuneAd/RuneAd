c3e2d1561f14401293a2420dd5900632d5d922f7
Aaron <Spaceage95@hotmail.com>
2020-09-09 21:47:09 -0500

    Update PagesController.php

--- app/controllers/PagesController.php
+++ app/controllers/PagesController.php
@@ -38,49 +38,46 @@ class PagesController extends Controller {
     }
 
     public function stats() {
-      $thisMonth = strtotime(date("Y-m-01 00:00:00"));
-      $lastMonth = strtotime("first day of last month");
-      $lastWeek = strtotime("-1 week +1 day");
-      $day = strtotime("today");
-      $hour = strtotime("-1 hour");
+        $thisMonth = strtotime(date("Y-m-01 00:00:00"));
+        $lastMonth = strtotime("first day of last month");
+        $lastWeek = strtotime("-1 week +1 day");
+        $day = strtotime("today");
+        $hour = strtotime("-1 hour");
 
+        $data = [
+            'users' => [
+                'total' => Users::count(),
+                'month' => Users::where("join_date", ">=", $thisMonth)->count()
+            ],
+            'votes' => [
+                'total' => Votes::count(),
+                'month' => Votes::where("voted_on", ">=", $thisMonth)->count(),
+                'lastmonth' => Votes::where("voted_on", ">=", $lastMonth)->count(),
+                'week' => Votes::where("voted_on", ">=", $lastWeek)->count(),
+                'day' => Votes::where("voted_on", ">=", $day)->count(),
+                'hour' => Votes::where("voted_on", ">=", $hour)->count()
 
-      $data = [
-          'users' => [
-              'total' => Users::count(),
-              'month' => Users::where("join_date", ">=", $thisMonth)->count()
-          ],
-          'votes' => [
-              'total' => Votes::count(),
-              'month' => Votes::where("voted_on", ">=", $thisMonth)->count(),
-              'lastmonth' => Votes::where("voted_on", ">=", $lastMonth)->count(),
-              'week' => Votes::where("voted_on", ">=", $lastWeek)->count(),
-              'day' => Votes::where("voted_on", ">=", $day)->count(),
-              'hour' => Votes::where("voted_on", ">=", $hour)->count()
+            ],
+            'reports' => [
+                'total' => Reports::count(),
+                'month' => Reports::where("date_reported", ">=", $thisMonth)->count()
+            ],
+            'payments' => [
+                'total' => Payments::sum('paid'),
+                'month' => Payments::where("date_paid", ">=", $thisMonth)->sum('paid'),
+            ],
+            'servers' => [
+                'total' => Servers::count(),
+                'month' => Servers::where("date_created", ">=", $thisMonth)->count(),
+            ]
+        ];
 
-          ],
-          'servers' => [
-              'total' => Servers::count(),
-              'month' => Servers::where("date_created", ">=", $thisMonth)->count(),
-          ]
-      ];
+        $chartData = Payments::getChartData();
 
-
-
-
-
-
-
+        $this->set("data", $data);
+        $this->set("chart_keys", array_keys($chartData));
+        $this->set("chart_values", array_values($chartData));
         return true;
-    }
-
-    public static function getChartData($start_date) {
-        return Votes::where("voted_on", '>', $start_date)
-            ->selectRaw('COUNT(*) AS total')
-            ->selectRaw("FROM_UNIXTIME(voted_on, '%m %d') AS time")
-            ->groupBy("time")
-            ->orderBy("time", 'ASC')
-            ->get();
     }
 
     public function commits() {
