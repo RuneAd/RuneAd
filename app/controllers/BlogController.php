f78b9702651c722792ddb09c5bb8fc9184287cb0
Aaron <Spaceage95@hotmail.com>
2020-09-11 14:20:45 -0500

    Update BlogController.php

--- app/controllers/BlogController.php
+++ app/controllers/BlogController.php
@@ -33,5 +33,72 @@ class BlogController extends Controller {
         $this->set("revisions", $revisions);
     	return true;
     }
+
+
+    public function upload() {
+        $file = $_FILES['image'];
+
+        $dims = getimagesize($file['tmp_name']);
+        if ($dims === false) {
+            return [
+                'success' => true,
+                'message' => 'File must be an image.'
+            ];
+        }
+
+        $mimes = ['jpg' => 'image/jpeg', 'gif' => 'image/gif', 'png' => 'image/png'];
+
+        $type   = mime_content_type($file['tmp_name']);
+        $ext    = pathinfo($file['name'])['extension'];
+        $size   = $file['size'];
+
+        $width  = $dims[0];
+        $height = $dims[1];
+
+        $maxDims = [468, 60];
+        $maxSize = (1024 * 1024 * 5);
+
+        if (!in_array($type, array_values($mimes))) {
+            return [
+                'success' => true,
+                'message' => 'Invalid file mime type.'
+            ];
+        }
+
+        if (!in_array($ext, array_keys($mimes))) {
+            return [
+                'success' => true,
+                'message' => 'Invalid file extension. Allowed: '.implode(', ', array_keys($mimes))
+            ];
+        }
+
+        if ($size > $maxSize) {
+            return [
+                'success' => true,
+                'message' => "Image can not exceed ".(($maxSize/1024)/1024)."MB."
+            ];
+        }
+
+        if ($width != $maxDims[0] && $height != $maxDims[1]) {
+            return [
+                'success' => true,
+                'message' => "Image must be $maxDims[0]px x $maxDims[1]px."
+            ];
+        }
+
+        $newname = md5($file['name'] . microtime()).'.'.$ext;
+
+        if (!move_uploaded_file($file['tmp_name'], 'public/img/blog/images/'.$newname)) {
+            return [
+                'success' => true,
+                'message' => 'Failed uploading file...'
+            ];
+        }
+
+        return [
+            'success' => true,
+            'message' => $newname,
+        ];
+    }
     
 }
