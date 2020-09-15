f298ea54447b7ef434114b46ab268fc5ef5f76e0
Aaron <Spaceage95@hotmail.com>
2020-09-15 14:01:44 -0500

    Update HomeAds1.js

--- public/js/HomeAds1.js
+++ public/js/HomeAds1.js
@@ -1,20 +1,20 @@
 $(document).ready(function() {
-    let Servers = [
+    let Servers1 = [
         '<a href="https://runead.com/ads" rel="nofollow" target="_self"><img alt="RuneAd" class="responsive-ad" src="https://runead.com/public/img/banners/banner468.png"></a>',
 
     ];
 
     let HomeAds1 = $('#HomeAds1');
 
-    let random = Math.floor(Math.random() * (Servers.length - 0)) + 0;
+    let random = Math.floor(Math.random() * (Servers1.length - 0)) + 0;
 
-    HomeAds1.html(Servers[random]);
+    HomeAds1.html(Servers1[random]);
 
     setInterval(function() {
-        random = Math.floor(Math.random() * (Servers.length - 0)) + 0;
+        random = Math.floor(Math.random() * (Servers1.length - 0)) + 0;
         $('#factBox div').fadeOut(function() {
             $(this).fadeIn();
-            HomeAds1.html(Servers[random]);
+            HomeAds1.html(Servers1[random]);
         })
     }, 6500);
 
