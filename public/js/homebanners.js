9b956874e9d496dcee7fc5f20cf1c688797ef328
Aaron <Spaceage95@hotmail.com>
2020-09-21 23:00:21 -0500

    s

--- public/js/homebanners.js
+++ public/js/homebanners.js
@@ -1,14 +1,25 @@
-function cycleImages(){
-    var $active = $('#cycler .active');
-    var $next = ($active.next().length > 0) ? $active.next() : $('#cycler img:first');
-    $next.css('z-index',2);//move the next image up the pile
-    $active.fadeOut(1500,function(){//fade out the top image
-    $active.css('z-index',1).show().removeClass('active');//reset the z-index and unhide the image
-        $next.css('z-index',3).addClass('active');//make the next image the top one
-    });
-  }
-
-$(document).ready(function(){
-// run every 7s
-setInterval('cycleImages()', 7000);
-})
+var slideShow = function(container, time) {
+    container = document.getElementById(container);
+    this.images = [];
+    this.curImage = 0;
+    for (i = 0; i < container.childElementCount; i++) {
+      this.images.push(container.children[i]);
+      this.images[i].style.opacity = 0;
+    }
+  
+    // Handle going to to the next slide
+    var nextSlide = function() {
+      for (var i = 0; i < this.images.length; i++) {
+        if (i!=this.curImage) this.images[i].style.opacity = 0;
+      }
+      this.images[this.curImage].style.opacity = 1;
+      this.curImage++;
+      if (this.curImage>=this.images.length) this.curImage=0;
+      window.setTimeout(nextSlide.bind(document.getElementById(this)), time);
+      // old code: window.setTimeout(nextSlide.bind(this), time);
+    };
+  
+    nextSlide.call(this);
+  
+  };
+  slideShow("slideshow", 8000);
