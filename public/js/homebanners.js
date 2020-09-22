22f2ce5487bec53288709ac15192adc9f4a9ee2f
Aaron <Spaceage95@hotmail.com>
2020-09-21 23:27:18 -0500

    Few updates

--- public/js/homebanners.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    var banners = [];
-    var index = 0;
-    banners[1] = '<div class="banners banner1">https://cdn.discordapp.com/attachments/722931780105011260/757807184229826560/728x90.gif</div>';
-    banners[2] = '<div class="banners banner2">https://cdn.discordapp.com/attachments/722931780105011260/757807298222489610/Comp_5.gif</div>';
-    index = Math.floor(Math.random() * banners.length);
-    $("#banner").html(banners[index]);
-   });
