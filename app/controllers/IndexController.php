ed914facbbb6b796ebf23b333bb2c9fc26a23165
Aaron <Spaceage95@hotmail.com>
2020-10-11 19:13:55 -0500

    Update IndexController.php

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -73,7 +73,7 @@ class IndexController extends Controller {
         if ($server->meta_tags)
             $this->set("meta_tags", implode(',',json_decode($server->meta_tags, true)));
 
-        $this->set("meta_info", $this->filter($server->meta_info '-' .$server_description));
+        $this->set("meta_info", $this->filter($server->meta_info));
         $this->set("seo_link", $seo);
 
         $body = str_replace("<img src", "<img data-src", $server->description);
