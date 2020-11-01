181960fe3b287fe951888c4555f63e399cdd6f9a
Aaron <Spaceage95@hotmail.com>
2020-11-01 16:08:10 -0600

    # of servers

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -52,6 +52,7 @@ class IndexController extends Controller {
         $this->set("servers", $servers);
         $this->set("revisions", $revisions);
         $this->set("sponsors", $sponsors);
+        $this->set("server_count", $servers->total());
     	return true;
     }
     public function details($id) {
