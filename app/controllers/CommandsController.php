3e1e163c06a4c88e94b9f45c940f69b7bbe04834
Aaron <Spaceage95@hotmail.com>
2020-12-25 17:17:09 -0600

    Update CommandsController.php

--- app/controllers/CommandsController.php
+++ app/controllers/CommandsController.php
@@ -4,6 +4,26 @@ class CommandsController extends Controller {
     public function index() {
         return true;
     }
+
+    public function owner() {
+        return true;
+    }
+
+    public function admin() {
+        return true;
+    }
+
+    public function moderator() {
+        return true;
+    }
+
+    public function donator() {
+        return true;
+    }
+
+    public function player() {
+        return true;
+    }
      
 }
 ?>
