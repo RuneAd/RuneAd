ce816a1c1cd714d57f20519c4f20e9bd9b911813
Aaron <Spaceage95@hotmail.com>
2021-01-25 21:54:26 -0600

    f

--- app/controllers/CommandsController.php
+++ app/controllers/CommandsController.php
@@ -2,6 +2,22 @@
 class CommandsController extends Controller {
 
     public function index() {
+        $turbos = Turbos::select([
+            'turbos.id',
+            'servers.title',
+            'servers.website',
+            'servers.discord_link',
+            'servers.banner_url'
+        ])
+        ->where('expires', '>', time())
+        ->where('servers.banner_url', '!=', null)
+        ->where('servers.website', '!=', null)
+        ->leftJoin("servers", "servers.id", "=", "turbos.server_id")
+        ->orderBy("started", "ASC")
+        ->get();
+
+        
+    $this->set("turbos", $turbos);
         return true;
     }
 
