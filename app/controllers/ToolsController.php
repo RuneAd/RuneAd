811da1fd90ddf683269edc33c902ddb00f5fc368
Aaron <Spaceage95@hotmail.com>
2021-01-25 20:51:27 -0600

    Update ToolsController.php

--- app/controllers/ToolsController.php
+++ app/controllers/ToolsController.php
@@ -12,6 +12,19 @@ class ToolsController extends Controller {
     }
 
     public function map() {
+        $turbos = Turbos::select([
+            'turbos.id',
+            'servers.title',
+            'servers.website',
+            'servers.discord_link',
+            'servers.banner_url'
+        ])
+        ->where('expires', '>', time())
+        ->where('servers.banner_url', '!=', null)
+        ->where('servers.website', '!=', null)
+        ->leftJoin("servers", "servers.id", "=", "turbos.server_id")
+        ->orderBy("started", "ASC")
+        ->get();
         return true;
     }
 
