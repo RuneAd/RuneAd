cdfb418f1387469e7216f160d59256b440ec1d9a
Aaron <Spaceage95@hotmail.com>
2021-02-01 21:23:15 -0600

    Update DashboardController.php

--- app/controllers/DashboardController.php
+++ app/controllers/DashboardController.php
@@ -1,242 +1,89 @@
 <?php
-use Fox\Request;
+use Illuminate\Pagination\Paginator;
 
 class DashboardController extends Controller {
-    
 
-    public function index($serverId, $incentive) {
-        $server = Servers::getServer($serverId);
+    public function index() {
+        $servers = Servers::where('owner', $this->user->user_id)->get();
+        $roles = implode(", ", json_decode($this->user->roles, true));
 
-        if (!$server) {
-           $this->setView("errors", "show404");
-           return false;
+        $idArr = array_column($servers->toArray(), "id");
+        $ids   = implode(",", $idArr);
+
+        if (count($servers) > 0) {
+            $votes = Votes::
+                select("*")
+                ->whereRaw("server_id IN (".$ids.")")
+                ->leftJoin("servers", "servers.id", "=", "votes.server_id")
+                ->orderByRaw("votes.voted_on DESC")
+                ->get();
         }
 
-        $ip   = $this->request->getAddress();
+        $votesArr = [];
 
-        $vote = Votes::query()
-            ->where("server_id", $server->id)
-            ->where(function($query) use ($ip, $incentive) {
-                $query
-                    ->where("ip_address", $ip)
-                    ->orWhere("incentive", $incentive);
-            })
-            ->whereRaw(time()." - voted_on < 43000")
-            ->first();
+        foreach ($idArr as $id) {
+            $votesArr[$id] = [
+                '1hour'    => 0,
+                '1day'     => 0,
+                '7days'    => 0,
+                '30days'   => 0,
+                '60days'   => 0,
+                'lifetime' => 0,
+            ];
+        }
 
+        if (count($servers) > 0) {
+            foreach($votes as $vote) {
+                $vote_time = $vote->voted_on;
+                $timeDiff  = time() - $vote_time;
 
-            $turbos = Turbos::select([
-                'turbos.id',
-                'servers.title',
-                'servers.website',
-                'servers.discord_link',
-                'servers.banner_url'
-            ])
-            ->where('expires', '>', time())
-            ->where('servers.banner_url', '!=', null)
-            ->where('servers.website', '!=', null)
-            ->leftJoin("servers", "servers.id", "=", "turbos.server_id")
-            ->orderBy("started", "ASC")
-            ->get();
-    
-            
-        $this->set("turbos", $turbos);
-        $this->set("incentive", $incentive);
-        $this->set("vote", $vote);
-        $this->set("server", $server);
-        $this->set("server_url", Functions::friendlyTitle($server->id.'-'.$server->title));
+                if ($timeDiff <= 3600) {
+                    $votesArr[$vote->server_id]['1hour']++;
+                }
+                if ($timeDiff <= 86400) {
+                    $votesArr[$vote->server_id]['1day']++;
+                }
+                if ($timeDiff <= 604800) {
+                    $votesArr[$vote->server_id]['7days']++;
+                }
+                if ($timeDiff <= 2592000) {
+                    $votesArr[$vote->server_id]['30days']++;
+                }
+                if ($timeDiff <= 10368000) {
+                    $votesArr[$vote->server_id]['60days']++;
+                }
+                $votesArr[$vote->server_id]['lifetime']++;
+            }
+        }
+        $this->set("roles", $roles);
+        $this->set("servers", $servers);
+        $this->set("voteData", $votesArr);
         return true;
     }
 
-    public function addvote() {
-        $id        = $this->request->getPost("server_id", "int");
-        $token     = $this->request->getPost("token");
-        $incentive = $this->request->getPost("incentive", "string");
-        $recaptcha = $this->verifyReCaptcha($token);
+    public function payments($page = 1) {
+          Paginator::currentPageResolver(function() use ($page) {
+               return $page;
+          });
+          $payments = Payments::where('user_id', $this->user->user_id)->paginate(15);
+          $sum = Payments::where('user_id', $this->user->user_id)->sum("paid");
+            $roles = implode(", ", json_decode($this->user->roles, true));
+            $this->set("payments", $payments);
+            return true;
+          }
 
-        if (!$recaptcha['success']) {
-            return [
-                'success' => false,
-                'message' => $recaptcha['error-codes'][0]
-            ];
-        }
+    public function stats() {
 
-        $server = Servers::getServer($id);
-
-        if (!$server) {
-            return [
-                'success' => false,
-                'message' => 'Invalid server id'
-            ];
-        }
-
-        $ip   = $this->request->getAddress();
-
-        $vote = Votes::query()
-            ->where("server_id", $server->id)
-            ->where(function($query) use ($ip, $incentive) {
-                if ($incentive) {
-                $query
-                    ->where("ip_address", $ip)
-                    ->orWhere("incentive", $incentive);
-                } else {
-                    $query
-                        ->where("ip_address", $ip);
-                }
-            })
-            ->whereRaw(time()." - voted_on < 43000")
-            ->first();
-
-        if ($vote) {
-            return [
-                'success' => false,
-                'message' => 'You have already voted within the last 12 hours!',
-                'votes'   => $server->votes
-            ];
-        }
-
-        $vote = new Votes();
-
-        $vote->fill([
-            'server_id'  => $server->id,
-            'ip_address' => $ip,
-            'incentive'  => $incentive,
-            'voted_on'   => time()
-        ]);
-
-        $saved = $vote->save();
-
-        if (!$saved) {
-            return [
-                'success' => false,
-                'message' => 'Vote failed to register.',
-                'votes' => $server->votes
-            ];
-        }
-
-        $server->votes += ($server->premium_expires > time() ? 1 : 1);
-        $server->save();
-
-        $votes = $server->votes;
-
-        if ($server->premium_expires > time()) {
-            $votes = ($votes + ($server->premium_level * 1));
-        }
-
-        if ($incentive != null && $server->callback_url) {
-            $callback = $this->sendIncentive($server->callback_url, $incentive);
-
-            if (isset($callback['success']) && isset($callback['message'])) {
-                return [
-                    'success' => $callback['success'],
-                    'message' => $callback['message']
-                ];
-            }
-        }
-
-        return [
-            'success' => true,
-            'message' => 'Thank you, your vote has been registered!',
-            'votes'   => $votes
-        ];
+        return true;
     }
 
-    /**
-     * @var string $token
-     * @return json
-     * Send post request containing token to validate request.
-     */
-    private function verifyReCaptcha($token){
-        $client = new GuzzleHttp\Client();
-
-        $response = $client->request('POST', "https://www.google.com/recaptcha/api/siteverify", [
-            'headers' => [
-                'Accept' => 'application/json'
-            ],
-            'form_params' => [
-                "secret"   => recaptcha['private'],
-                "response" => $token
-            ]
-        ]);
-
-        $json = json_decode($response->getBody(), true);
-        return $json;
-    }
-
-    /**
-     * Sends a get request to specified callback url.
-     * Used primarily for vote scripts.
-     */
-    public function sendIncentive($url, $incentive) {
-        // if url ends with an = (means it's expecting a value right after, then just append the incentive)
-        if ($this->endsWith($url, '=')) {
-			$url = $url.$incentive;
-	    } else {
-	    	$isFile = substr($url, strlen($url) - 4, strlen($url)) == ".php";
-
-	    	if ($isFile) {
-	    		$url = $url.'?postback='.$incentive;
-	    	} else {
-	    		$hasSep = substr($url, strlen($url) - 1, strlen($url)) == "/";
-	    		$url = $url.($hasSep ? '' : '/').$incentive;
-	    	}
-        }
-
-        try {
-            $client = new GuzzleHttp\Client();
-
-            $response = $client->request('GET', $url, [
-                'headers' => [
-                    'Accept' => 'application/json',
-                    'Upgrade-Insecure-Requests' => 1,
-                    'User-Agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36',
-                    'Accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
-                    'Accept-Language' => 'en-US,en;q=0.9'
-                ],
-            ]);
-
-            $json = $response->getBody();
-
-            return [
-                'http_code' => $response->getStatusCode(),
-                'response' => json_decode($json, true)
-            ];
-        } catch (Exception $e) {
-            return null;
-        }
-    }
-
-    /**
-     * @var $url
-     * return bool
-     * Returns true if url contains a get query.
-     */
-    private function hasQuery($url) {
-    	return strpos($url, '?') !== false || strpos($url, '&') !== false;
-    }
-
-    /**
-     * @var string $string
-     * @var string $search
-     * Returns true if givvn string contains search param
-     */
-    private function endsWith($string, $search) {
-	    $length = strlen($string);
-
-	    if ($length == 0) {
-	        return false;
-	    }
-
-	    return substr($string, $length - 1, $length) == $search;
-    }
+    public $access =  [
+        'login_required' => true,
+        'roles'  => ['member', 'moderator', 'admin']
+    ];
 
     public function beforeExecute() {
-        if ($this->getActionName() == "addvote") {
-            $this->request = Request::getInstance();
-            $this->disableView(true);
-            return true;
-        }
-
         return parent::beforeExecute();
     }
+
 }
